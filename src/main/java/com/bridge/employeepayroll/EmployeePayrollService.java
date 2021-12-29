a270a091d58c80ecdbd52ca15fbb7abfa39abdd2
Navneet <navn0306@gmail.com>
2021-12-29 23:31:00 +0530

    ADD: UC1, Created an employee payroll service to read and write employee data from console

---
+++ src/main/java/com/bridge/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,41 @@
+package com.bridge.employeepayroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+
+    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+
+    public EmployeePayrollService() {
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData>
+                                          employeePayrollList) {}
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+
+        System.out.println("Enter the Emp ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Emp Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Emp Salary");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\n Writing employee payroll roster to console\n" +employeePayrollList);
+    }
+}
---
+++ src/main/java/com/bridge/fileutil/FileUtil.java
@@ -0,0 +1,17 @@
+package com.bridge.fileutil;
+
+import java.io.File;
+
+public class FileUtil {
+
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
